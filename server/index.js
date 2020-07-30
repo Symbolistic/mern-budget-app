460b4d5758cd4c194dc0a2e8b54164af73542de6
Symbol <Symbolistic@live.com>
2020-07-30 16:27:54 -0400

    trying again...

--- server/index.js
+++ server/index.js
@@ -23,7 +23,15 @@ app.use('/api/income', require('./routes/income'));
 app.use('/api/savings', require('./routes/savings'));
 app.use('/api/expense', require('./routes/expense'));
 
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
 
+    //index.html for all page routes
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
