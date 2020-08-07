4b29c68396c669fc7053b383c84077f48a1d8567
Symbol <Symbolistic@live.com>
2020-08-07 10:20:32 -0400

    forgot to add new mongo uri variable, the updated one, fixed it, trying again

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 require("dotenv").config();
 //const config = require("./config/key");
 
-mongoose.connect(process.env.mongoURI,
+mongoose.connect(process.env.MONGO_URI,
                  { useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true }).then(() => console.log("Connected to Database"))
                                                                     .catch(err => console.log(err));
 
