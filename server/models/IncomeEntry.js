51bb6495e1231c9ce4e6ba3ce78761393ca5a0b5
Symbol <Symbolistic@live.com>
2020-09-30 15:58:34 -0400

    added to index.js to enforce/redirect to https

--- server/models/IncomeEntry.js
+++ server/models/IncomeEntry.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const incomeEntrySchema = mongoose.Schema({
-  entryFrom: {
-    type: Schema.Types.ObjectId,
-    ref: "IncomeGroup",
-    immutable: true,
-  },
-  description: { type: String, required: true },
-  amount: { type: Number, required: true },
-  actual: { type: Number, default: 0 },
+	entryFrom: {
+		type: Schema.Types.ObjectId,
+		ref: 'IncomeGroup',
+		immutable: true,
+	},
+	description: { type: String, required: true },
+	amount: { type: Number, required: true },
+	actual: { type: Number, default: 0 },
 });
 
-const IncomeEntry = mongoose.model("IncomeEntry", incomeEntrySchema);
+const IncomeEntry = mongoose.model('IncomeEntry', incomeEntrySchema);
 
 module.exports = { IncomeEntry };
